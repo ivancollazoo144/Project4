4e15bfb6b14939214d5d9e1ab57572a635fd5b4c
ivancollazoo <collazoivan1412@gmail.com>
2025-01-29 22:04:57 -0400

    Java is for me code

--- src/demo1/DemoTest4.java
+++ src/demo1/DemoTest4.java
@@ -3,7 +3,15 @@ package demo1;
 public class DemoTest4 {
 public static void main(String[] args) {
 		
+		int a = 100;
+		int b = 200;
+		
+		if (a < b) {
+			System.out.println("Java is for me");
+		} else {
+			
 		
 		System.out.println("Java");
+		}
 	}
 }
